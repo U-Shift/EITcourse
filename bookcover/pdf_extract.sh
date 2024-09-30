fc8a3424b1c3e8ddb8c46f522d122d708875380c
RFlx <temospena@gmail.com>
2024-09-30 18:03:04 +0100

    pdf extract and merge

--- bookcover/pdf_extract.sh
+++ bookcover/pdf_extract.sh
@@ -1,10 +1,10 @@
 #install qpdf
 # sudo apt install qpdf
-cd _book
-
-# delete from page 3 to the end of covertitle.pdf
-# qpdf /media/rosa/Dados/GIS/Qtests/_book/covertitle.pdf --pages . 1-2 -- /media/rosa/Dados/GIS/Qtests/_book/output.pdf
 
 
 # merge and extract pages
-qpdf EITcourse_GetYourDatasetReady2024.pdf --pages covertitle.pdf 1-2 EITcourse_GetYourDatasetReady2024.pdf 2-z -- result.pdf  
+qpdf _book/GetYourDatasetReady2024.pdf --pages bookcover/_book/covertitle.pdf 1-2 _book/GetYourDatasetReady2024.pdf 2-z -- _book/EITcourse_GetYourDatasetReady2024.pdf  
+
+
+# upload to github
+Rscript -e "piggyback::pb_upload('_book/EITcourse_GetYourDatasetReady2024.pdf')"
